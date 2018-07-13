918a465fdb9fbc6a7baffa8bacc14432c49505c4
Igor Ryabchuk <riabtchuk@gmail.com>
2018-07-13 17:03:01 +0100

    Amend index view and test to display title

--- app/cypress/integration/basic_spec.js
+++ app/cypress/integration/basic_spec.js
@@ -2,5 +2,6 @@ describe('Index page', () => {
   it('should render "I am up and running"', () => {
     cy.visit('http://localhost:3000')
     cy.contains('I am up and running')
+    cy.contains('FT Tech Tester')
   })
 })
